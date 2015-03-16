e15771e5710a48fbc3c2a8672b1cea88323c9963
soccerKevin <soccerkevint11@gmail.com>
2015-03-16 15:57:54 -0700

    Made a static page

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'minitest-rails'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -43,3 +46,6 @@ group :development, :test do
   gem 'spring'
 end
 
+group :test do
+  gem 'minitest-rails-capybara'
+end
---
+++ test/features/article/crud_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper.rb'
+
+
+feature "As a developer I want all parts of crud to work on my blog" do 
+  scenario "As a blog owner, I want to show all blog entries" do
+    
+  end
+
+  scenario "As a blog owner, I want to create a new article" do
+  end
+
+  scenario "As a blog owner, I want to edit an existing article" do
+  end
+
+  scenario "As a blog owner, I want to delete existing article" do
+  end
+end
