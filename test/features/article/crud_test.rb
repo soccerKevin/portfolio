b5ca2b25c694979cfaa6324d0a919f1b95c82727
soccerKevin <soccerkevint11@gmail.com>
2015-03-16 16:34:18 -0700

    articles complete

--- test/features/article/crud_test.rb
+++ test/features/article/crud_test.rb
@@ -2,17 +2,52 @@ require 'test_helper.rb'
 
 
 feature "As a developer I want all parts of crud to work on my blog" do 
+  scenario "As a blog owner, I want to index all blog entries" do
+    #given I visit the articles page
+    visit articles_path
+
+    #then I should see some articles
+    page.text.must_include "Listing Articles"
+  end
+
   scenario "As a blog owner, I want to show all blog entries" do
-    
-    
+    #given I visit the articles page
+    visit article_path articles(:first)
+
+    #then I should see some articles
+    page.text.must_include "A Title"
   end
 
   scenario "As a blog owner, I want to create a new article" do
+    #when I fill out a new article form
+    visit new_article_path
+    fill_in "Title", with: "A Title"
+    fill_in "Body", with: "Some content here"
+
+    #when I submit article 
+    click_on "Create Article"
+
+    #then I should have a new article
+    page.text.must_include "Article was successfully created"
   end
 
   scenario "As a blog owner, I want to edit an existing article" do
+    #given I have an existing article
+    visit edit_article_path articles(:first)
+    fill_in "Title", with: "A Different Title"
+
+    #when I edit the article
+    click_on "Update Article"
+
+    #then the article should be updated
+    page.text.must_include "Article was successfully updated"
   end
 
   scenario "As a blog owner, I want to delete existing article" do
+    #given that I have an existing article
+    visit articles_path
+    click_on "Destroy"
+
+    page.text.must_include "Article was successfully destroyed"
   end
 end
