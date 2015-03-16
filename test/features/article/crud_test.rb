9baa3fac5aa56db596800cf058144c6a0d119bb9
soccerKevin <soccerkevint11@gmail.com>
2015-03-16 16:05:48 -0700

    generated scaffold article

--- test/features/article/crud_test.rb
+++ test/features/article/crud_test.rb
@@ -4,6 +4,7 @@ require 'test_helper.rb'
 feature "As a developer I want all parts of crud to work on my blog" do 
   scenario "As a blog owner, I want to show all blog entries" do
     
+    
   end
 
   scenario "As a blog owner, I want to create a new article" do
