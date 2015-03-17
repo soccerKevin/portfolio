b3ebef9ffb3eb92d13658f24d5c492e9e6be1d78
soccerKevin <soccerkevint11@gmail.com>
2015-03-17 14:14:27 -0700

    cleaned some testers up

---
+++ test/features/auth/accounts_connected_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+feature "As a user I want my articles connected to my account so that I can be attributed as author" do
+  scenario "As a signed in user, I can edit articles" do
+    #given I'm signed in
+    visit user_session_path
+    sign_in @user
+    puts page.html
+
+  end
+end
