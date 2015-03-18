723babd11fea25035bb98e95f8491c0e92652b3e
soccerKevin <soccerkevint11@gmail.com>
2015-03-17 23:17:07 -0700

    Devise is finished here I think

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  #before_action :authenticate_user!
+  has_many :articles, foreign_key: "author_id"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
