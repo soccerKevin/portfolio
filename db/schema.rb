62a388eeea3f14f9a57e1f469a17c1f3b9050b1b
soccerKevin <soccerkevint11@gmail.com>
2015-04-14 16:21:53 -0700

    migrated, changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405053820) do
+ActiveRecord::Schema.define(version: 20150405192420) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20150405053820) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
