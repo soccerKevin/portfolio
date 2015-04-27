8b62f59f926f9ab5b57ada7ff73dcd6abc8017c2
soccerKevin <soccerKevint11@gmail.com>
2015-04-26 22:11:21 -0700

    add provider and uid to omniauth users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405192420) do
+ActiveRecord::Schema.define(version: 20150427051043) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20150405192420) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
