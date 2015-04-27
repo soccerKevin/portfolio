0b91c536ee2ec243371b12fe853ad23d5bf03d93
soccerKevin <soccerKevint11@gmail.com>
2015-04-27 10:24:29 -0700

    setup users to handle twitter callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,39 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :articles, foreign_key: "author_id"
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.from_omniauth(auth)
+    where(auth.slice(provider: auth.provider, uid: auth.uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session['devise.user_attributes']
+      new(session['devise.user_attributes'], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provuder.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 
   def editor?
     role == "editor"
