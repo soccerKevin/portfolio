5a067f95112a1172f1885739c8b8bc96acdefb4b
soccerKevin <soccerKevint11@gmail.com>
2015-04-30 12:26:15 -0700

    validate email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
 	validates_presence_of :name
-	validates :from_email, presence: true, email: true
+	validates :from_email, presence: true, uniqueness: true, email: true
 	validates_presence_of :body
 end 
