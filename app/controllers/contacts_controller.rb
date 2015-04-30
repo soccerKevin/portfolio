d1135b9bbc7d858e938feeb16853125b0b7399ef
soccerKevin <soccerKevint11@gmail.com>
2015-04-29 18:03:39 -0700

    contact new working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,16 @@
 class ContactsController < ApplicationController
+	def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+		@status = "Thank you for your message"
+		render html: :new
+	end
+
+private
+	def contact_params
+		params.require(:contact).permit(:name, :email, :body)
+	end
 end
