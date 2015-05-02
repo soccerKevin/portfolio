80923809ae602892ba65d66205deb89672ad8ae7
soccerKevin <soccerKevint11@gmail.com>
2015-05-01 21:13:38 -0700

    add current page class to body

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,15 @@
 class StaticController < ApplicationController
+	before_action :body_class
+
   def index
   end
 
   def about_me
+  	@page_name = "about_me"
   end
+
+ private
+ 	def body_class
+ 		set_tab action_name.to_sym
+ 	end
 end
