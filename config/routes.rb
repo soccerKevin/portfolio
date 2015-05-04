3fc5dacfd3e97df523e2bc42026860ac5d13b4b7
soccerKevin <soccerKevint11@gmail.com>
2015-05-04 12:04:30 -0700

    limit project routes to index

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
                      controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
-  resources :projects
+  resources :projects only: :index
 
   resources :articles do
     resources :comments
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,139 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+projects = Project.create([
+	{
+		name: 'Gaelsong',
+		pic_url: 'gaelsong.jpg',
+		site_url: "http://gaelsong.townsendwebdd.com",
+		categories: 'CODE FAVORITE DESIGN GENERAL'
+	},
+
+	{
+		name: 'Braces Painter',
+		pic_url: 'braces-painter.jpg',
+		site_url: "http://www.fairbanksorthodontics.com/brace-painter",
+		categories: 'CODE FAVORITE SESAME'
+	},
+
+	{
+		name: 'Color Retainer',
+		pic_url: 'color-retainer.jpg',
+		site_url: "http://www.forrestortho.com/color-your-retainer",
+		categories: 'CODE SESAME'
+	},
+
+	{
+		name: 'Avengers Movie Poster',
+		pic_url: 'avengersPoster.jpg',
+		site_url: "/assets/images/avengers.jpg",
+		categories: 'DESIGN FAVORITE'
+	},
+
+	{
+		name: 'Greenbay Webite Remake',
+		pic_url: 'greenbay.jpg',
+		site_url: "http://greenbay.townsendwebdd.com",
+		categories: 'DESIGN'
+	},
+
+	{
+		name: 'NuBots Magazne Cover',
+		pic_url: 'nuBots2.jpg',
+		site_url: "/assets/images/nano.jpg",
+		categories: 'DESIGN'
+	},
+
+	{
+		name: 'Playable Tetris',
+		pic_url: 'tetris.jpg',
+		site_url: "http://tetris.townsendwebdd.com",
+		categories: 'CODE FAVORITE'
+	},
+
+	{
+		name: 'Hanford Downwinders videosite',
+		pic_url: 'hanford.jpg',
+		site_url: "http://hanford.townsendwebdd.com",
+		categories: 'CODE SESAME GENERAL'
+	},
+
+	{
+		name: 'Giannini and Gray Dental Prtners',
+		pic_url: 'bgray.jpg',
+		site_url: "http://www.smiledc.com",
+		categories: 'CODE FAVORITE SESAME'
+	},
+
+	{
+		name: 'Braces Diagram',
+		pic_url: 'diagram.jpg',
+		site_url: "http://www.smilewny.com/braces-diagram",
+		categories: 'CODE SESAME'
+	},
+
+	{
+		name: 'Gary Login ajax-mailer',
+		pic_url: 'login.jpg',
+		site_url: "http://www.garylogin.com",
+		categories: 'CODE SESAME SESAME'
+	},
+
+	{
+		name: 'Liberty Template',
+		pic_url: 'liberty.jpg',
+		site_url: "http://www.libertyortho.ca",
+		categories: 'CODE GENERAL SESAME'
+	},
+
+	{
+		name: 'Bay View Orthodontics',
+		pic_url: 'bayViewOrtho.jpg',
+		site_url: "http://www.bayviewortho.com",
+		categories: 'DESIGN SESAME GENERAL'
+	},
+
+	{
+		name: 'UW Dental',
+		pic_url: 'uwDental.jpg',
+		site_url: "http://www.udadental.com",
+		categories: 'DESIGN SESAME GENERAL'
+	},
+
+	{
+		name: 'The Place To Brace',
+		pic_url: 'placeToBrace.jpg',
+		site_url: "http://www.theplacetobrace.com",
+		categories: 'SESAME GENERAL'
+	},
+
+	{
+		name: 'Baesel Ortho Fixed Bar',
+		pic_url: 'baesel.jpg',
+		site_url: "http://www.baeselortho.com",
+		categories: 'CODE SESAME GENERAL'
+	},
+
+	{
+		name: 'Sindelar Dental',
+		pic_url: 'sindelarDental.jpg',
+		site_url: "http://www.sindelardental.com",
+		categories: 'CODE FAVORITE SESAME'
+	},
+
+	{
+		name: 'Stevenson (Home Page)',
+		pic_url: 'stevenson.jpg',
+		site_url: "http://www.rjstevensondds.com/index.php",
+		categories: 'CODE SESAME GENERAL'
+	},
+
+	{
+		name: 'Jarvis Orthodontics',
+		pic_url: 'jarvisOrtho.jpg',
+		site_url: "http://www.jarvisortho.com",
+		categories: 'DESIGN SESAME GENERAL'
+	}
+])
