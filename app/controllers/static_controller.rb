a813a7268d1645d84ad34a570e75d55aa55ef425
soccerKevin <soccerKevint11@gmail.com>
2015-05-12 15:43:17 -0700

    move @social_icons to the controller
    where is belongs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,12 @@
 class StaticController < ApplicationController
 
   def index
+  	@social_icons = [
+				{name: 'linkedin', link: "https://www.linkedin.com/in/townsenddev" },
+				{name: 'twitter', link: "https://twitter.com/SoccerKevin11" },
+				{name: 'github', link: "https://github.com/soccerKevin" },
+				{name: 'blog', link: "/articles" }
+			]
   end
 
   def about_me
