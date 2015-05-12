5619d92b593cbfcf07e1c7c87ea46378bdf96b21
soccerKevin <soccerKevint11@gmail.com>
2015-05-12 16:48:34 -0700

    skill sets in controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -9,7 +9,21 @@ class StaticController < ApplicationController
 			]
   end
 
-  def about_me
+  def about
   	@page_name = "about_me"
+    @skill_set1 = [
+      {name: 'HTML5', rank: '95', color: 'red'},
+      {name: 'HAML', rank: '85', color: 'red'},
+      {name: 'CSS3', rank: '95', color: 'green'},
+      {name: 'BOOTSTRAP', rank: '85', color: 'blue'},
+      {name: 'Git', rank: '85', color: 'gray'}
+    ]
+    @skill_set2 = [
+      {name: 'JavaScript', rank: '90', color: 'blue'},
+      {name: 'AngularJS', rank: '85', color: 'green'},
+      {name: 'Coffee Script', rank: '85', color: 'brown'},
+      {name: 'Ruby/Rails', rank: '80', color: 'red'},
+      {name: 'BDD', rank: '80', color: 'green'}
+    ]
   end
 end
