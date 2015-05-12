c7340b6fd4c34e5f40846c25f3acc381448b3ed4
soccerKevin <soccerKevint11@gmail.com>
2015-05-11 17:53:20 -0700

    fixing facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.from_omniauth(auth)
-    puts auth.inspect
     auth_search = {
       email: auth.info.email
     }
+    puts auth_search
 
     # auth.slice(email: auth.info.email, provider: auth.provider, uid: auth.uid)
     where(auth_search).first_or_create do |user|
