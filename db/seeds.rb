5491b0bc39e07384cf9fcccdd299de040f2e2362
soccerKevin <soccerKevint11@gmail.com>
2015-05-12 19:20:12 -0700

    add tv_tracker and invoicer to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,130 +14,124 @@ projects = Project.create([
 		site_url: "http://gaelsong.townsendwebdd.com",
 		categories: 'CODE FAVORITE DESIGN GENERAL'
 	},
-
 	{
 		name: 'Braces Painter',
 		pic_url: 'braces-painter.jpg',
 		site_url: "http://www.fairbanksorthodontics.com/brace-painter",
-		categories: 'CODE FAVORITE SESAME'
+		categories: 'CODE FAVORITE WORK'
 	},
-
 	{
 		name: 'Color Retainer',
 		pic_url: 'color-retainer.jpg',
 		site_url: "http://www.forrestortho.com/color-your-retainer",
-		categories: 'CODE SESAME'
+		categories: 'CODE WORK'
 	},
-
 	{
 		name: 'Avengers Movie Poster',
 		pic_url: 'avengersPoster.jpg',
 		site_url: "avengers.townsendwebdd.com",
 		categories: 'DESIGN FAVORITE'
 	},
-
 	{
 		name: 'Greenbay Webite Remake',
 		pic_url: 'greenbay.jpg',
 		site_url: "http://greenbay.townsendwebdd.com",
 		categories: 'DESIGN'
 	},
-
 	{
 		name: 'NuBots Magazne Cover',
 		pic_url: 'nuBots2.jpg',
 		site_url: "nano.townsendwebdd.com",
 		categories: 'DESIGN'
 	},
-
 	{
 		name: 'Playable Tetris',
 		pic_url: 'tetris.jpg',
 		site_url: "http://tetris.townsendwebdd.com",
 		categories: 'CODE FAVORITE'
 	},
-
 	{
 		name: 'Hanford Downwinders videosite',
 		pic_url: 'hanford.jpg',
 		site_url: "http://hanford.townsendwebdd.com",
-		categories: 'CODE SESAME GENERAL'
+		categories: 'CODE WORK GENERAL'
 	},
-
 	{
 		name: 'Giannini and Gray Dental Prtners',
 		pic_url: 'bgray.jpg',
 		site_url: "http://www.smiledc.com",
-		categories: 'CODE FAVORITE SESAME'
+		categories: 'CODE FAVORITE WORK'
 	},
-
 	{
 		name: 'Braces Diagram',
 		pic_url: 'diagram.jpg',
 		site_url: "http://www.smilewny.com/braces-diagram",
-		categories: 'CODE SESAME'
+		categories: 'CODE WORK'
 	},
-
 	{
 		name: 'Gary Login ajax-mailer',
 		pic_url: 'login.jpg',
 		site_url: "http://www.garylogin.com",
-		categories: 'CODE SESAME SESAME'
+		categories: 'CODE WORK'
 	},
-
 	{
 		name: 'Liberty Template',
 		pic_url: 'liberty.jpg',
 		site_url: "http://www.libertyortho.ca",
-		categories: 'CODE GENERAL SESAME'
+		categories: 'CODE GENERAL WORK'
 	},
-
 	{
 		name: 'Bay View Orthodontics',
 		pic_url: 'bayViewOrtho.jpg',
 		site_url: "http://www.bayviewortho.com",
-		categories: 'DESIGN SESAME GENERAL'
+		categories: 'DESIGN WORK GENERAL'
 	},
-
 	{
 		name: 'UW Dental',
 		pic_url: 'uwDental.jpg',
 		site_url: "http://www.udadental.com",
-		categories: 'DESIGN SESAME GENERAL'
+		categories: 'DESIGN WORK GENERAL'
 	},
-
 	{
 		name: 'The Place To Brace',
 		pic_url: 'placeToBrace.jpg',
 		site_url: "http://www.theplacetobrace.com",
-		categories: 'SESAME GENERAL'
+		categories: 'WORK GENERAL'
 	},
-
 	{
 		name: 'Baesel Ortho Fixed Bar',
 		pic_url: 'baesel.jpg',
 		site_url: "http://www.baeselortho.com",
-		categories: 'CODE SESAME GENERAL'
+		categories: 'CODE WORK GENERAL'
 	},
-
 	{
 		name: 'Sindelar Dental',
 		pic_url: 'sindelarDental.jpg',
 		site_url: "http://www.sindelardental.com",
-		categories: 'CODE FAVORITE SESAME'
+		categories: 'CODE FAVORITE WORK'
 	},
-
 	{
 		name: 'Stevenson (Home Page)',
 		pic_url: 'stevenson.jpg',
 		site_url: "http://www.rjstevensondds.com/index.php",
-		categories: 'CODE SESAME GENERAL'
+		categories: 'CODE WORK GENERAL'
 	},
-
 	{
 		name: 'Jarvis Orthodontics',
 		pic_url: 'jarvisOrtho.jpg',
 		site_url: "http://www.jarvisortho.com",
-		categories: 'DESIGN SESAME GENERAL'
+		categories: 'DESIGN WORK GENERAL'
+	},
+	{
+		name: 'TV Tracker',
+		pic_url: 'tv_tracker.jpg',
+		site_url: "http://tv-tracker.herokuapp.com",
+		categories: 'RUBY FAVORITE GENERAL'
+	},
+	{
+		name: 'Invoice.re',
+		pic_url: 'invoice.jpg',
+		site_url: "https://www.invoice.re/",
+		categories: 'RUBY FAVORITE GENERAL'
 	}
 ])
